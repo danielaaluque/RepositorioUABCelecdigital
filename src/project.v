34eb9bc2b4442141fd2588bd88000c2996e1922b
danielaaluque <daniela.aguirre.luque@uabc.edu.mx>
2025-10-23 18:26:17 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_prueba (
+module tt_um_relax (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
